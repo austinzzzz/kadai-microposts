08ff2013f3be5660831c381e697961140a29ef87
Austin Z <ec2-user@ip-172-31-14-59.us-east-2.compute.internal>
2018-06-06 06:18:17 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// user registration
+
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
 Route::group(['middleware' => ['auth']], function () {
-Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+   Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+   Route::get('/', 'MicropostsController@index');
+   Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
